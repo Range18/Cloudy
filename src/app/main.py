617c32ca23591e901bba832d3cb06be07b0b68e0
Range18 <shakmate@mail.ru>
2025-05-16 08:46:56 +0500

    feat(main.py): add -h and --help args

--- src/app/main.py
+++ src/app/main.py
@@ -9,6 +9,12 @@ from file_handler import FileHandler
 def main():
     args = sys.argv[1:]
 
+    if "-h" in args or "--help" in args:
+        print("Usage: python main.py [OPTIONS]")
+        print("Options:")
+        print("  -h, --help")
+        exit(0)
+
     event_handler = FileHandler()
     observer = Observer()
     observer.schedule(event_handler, 'D:\PyProjects\Cloud\\test', recursive=True)
